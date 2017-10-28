00f4e80f8b0ba887a378f04c68118be8ae9dbba0
David Škorjanc <dasko1988@gmail.com>
2017-10-28 11:32:54 +0200

    abilitato service registryt

--- src/main/java/it/ts/dotcom/example/eureka/EurekaApplication.java
+++ src/main/java/it/ts/dotcom/example/eureka/EurekaApplication.java
@@ -2,11 +2,14 @@ package it.ts.dotcom.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//abilita eureka service registry (dashboard: localhost:8761)
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 }
